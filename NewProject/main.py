5fc83407324a2a228d98ce5a8bd9df4adc6e1fe2
RoshanDPEA <114439704+RoshanDPEA@users.noreply.github.com>
2023-01-25 14:49:25 -0800

    Update main.py

--- NewProject/main.py
+++ NewProject/main.py
@@ -2,6 +2,7 @@ import os
 import spidev
 import RPi.GPIO as GPIO
 
+#import time
 from datetime import datetime
 from time import sleep
 from threading import Thread
@@ -27,6 +28,8 @@ from pidev.kivy import ImageButton
 from Slush.Devices import L6470Registers
 
 cyprus.initialize()
+cyprus.setup_servo(1)  # sets up P4 on the RPiMIB as an RC servo style output
+cyprus.set_servo_position(1, 0.5)
 
 time = datetime
 spi = spidev.SpiDev()
@@ -38,39 +41,6 @@ SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
 ADMIN_SCREEN_NAME = 'admin'
 
-
-"""
-
-#Adjust spiral rotating disk posistion to control amplitude
-#adjust disk rotate speed in sync to adjust freqency
-
-#equation for amplitude can be calculated by manually applying a gradient along the sprial
-# using that as the variable value and multiplying that by the change constant which is the
-# amount the spiral shrinks in radius with each loop with max diameter of the spiral and the
-# calibration to get the posistion we can use that to caculate the amplitude
-
-def update_current_position():
-    #make the color gradient corespond to apmpited then convert amplitude to poition on the spiral
-    self.current_position = self.color_gradient_color*self.sclar_that_realtes_color_gradient_to_position on spiral
-def sekiton_function_amplitude_control(amplitude):
-    #convert desired ampliude to position on spiral and use position control to move the spiral to desired position
-    self.current_amplitude = self.max_radius - (self.current_position*self.radius_decrease_per_position_change)
-
-#increase or decrease speed to adjust the position of the disk on the spiral
-def position_control(position):
-    #calculate the distance traveled during the aceleration set the aceleration value constant with the stepper motor
-    #set a value that increases or decreases speed by x value befre hand and increases or decreases speed when adjusting disk position
-    #using the calculated distance traveled during aceleration/deceleration and convert that to am amplitude change then set that as a minimum change in amplitudeto make a change
-    #set up a y = m*(x-(a+b)) function
-    #where y is time, m is the distance traveled after reaching speed, x is desired distance to position on spiral,
-    # a is distance change when accelerating, and b is distance change when decelerating
-    
-def frequency_control():
-    #increase or decrease speed to increase freqency 
-    
-    
-    
-"""
 
 class ProjectNameGUI(App):
     """
@@ -96,6 +66,8 @@ s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_curr
 s1 = stepper(port=1, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
              steps_per_unit=200, speed=2)
 
+global s0_rotation_direction
+global s1_rotation_direction
 s0_rotation_direction = 0
 s1_rotation_direction = 1
 
@@ -105,6 +77,9 @@ class MainScreen(Screen):
         super().__init__(**kw)
 
         self.motor_0_speed = 0
+        self.current_spiral_position = 0
+        self.freqency_to_motor_speed = 0
+        self.freqency = 0
 
         """Things that are actually happening when the MainScreen class is called
         These variables are only defined here so they can be altered later.
@@ -113,11 +88,6 @@ class MainScreen(Screen):
         and cancel the clock until the value is returned to 0, which the_dance function does when it is finished running"""
 
     def move(self, MotorNumber, rotation_direction):
-
-        """General move function for different motors. This will check if both s0 and s1 are busy as it is currently
-        being used to turn two motors simultaneously and should never need to turn one on independently of the other.
-        This can be changed by changing the bellow if statement. The rotation direction is also controlled here. The
-        speed is relative to a slider, in this case slider 1."""
 
         if not s0.is_busy() or not s1.is_busy():
             MotorNumber.go_until_press(rotation_direction, self.ids.speed_slider_1.value)
@@ -128,8 +98,7 @@ class MainScreen(Screen):
             print("s0: I'm softStopped!")
 
     def move_both(self):
-
-        """Function that calls the move function twice. This button can turn the motors on or off."""
+        # moves both motors and turns them off.
 
         if not s0.is_busy() or not s1.is_busy():
             self.move(s0, s0_rotation_direction)
@@ -143,12 +112,89 @@ class MainScreen(Screen):
             s1.softStop()
             print("s1. I'm softStopped!")
 
-    def speed_change(self):
 
-        """Updates the speed of both motors. This will also sync both motors as the "motor_0_speed" which is
-        controlled by the "add_10" and "subtract_10" functions will be overwritten by running this function. This
-        works well, and without changing the other functions this should stay here. The other portions checks if the
-        motors are running and changes the speed if they are, and will do nothing if they are not spinning."""
+    def rayne_test_thing(self):
+
+
+        s0.go_until_press(s0_rotation_direction, 10000)
+        s1.go_until_press(s1_rotation_direction, 10000)
+
+        sleep(5)
+
+        print("slowing")
+        s0.go_until_press(s0_rotation_direction, 7600)
+        sleep(4.5)
+        print("speeding up")
+        s0.go_until_press(s0_rotation_direction, 10000)
+        self.current_spiral_position = 1
+
+    def specific_control(self):
+        #goes halway on spiral
+        print("slowing")
+        s0.go_until_press(s0_rotation_direction, 7600)
+        sleep(2.25)
+        print("speeding up")
+        s0.go_until_press(s0_rotation_direction, 10000)
+        self.current_spiral_position = .5
+
+    def spiral_position_control(self, spiral_pos):
+        #spiral position in fraction of the whole
+        wait_time = 4.5*(spiral_pos - self.current_spiral_position)
+        if wait_time > 0:
+            print("slowing")
+            s0.go_until_press(s0_rotation_direction, 7600)
+            sleep(wait_time)
+        else:
+            print("speeding up")
+            s0.go_until_press(s0_rotation_direction, 10000 + (10000-7600))
+            sleep(wait_time * -1)
+        print("balancing speed")
+        s0.go_until_press(s0_rotation_direction, 10000)
+
+    def amplitude_control(self, amplitude):
+        # spiral position in fraction of the whole
+
+        #replace this to show current amplitude
+        current_amplitude = 0
+
+        amount_to_move = amplitude - current_amplitude
+
+        #calculate amount of revolutions to get to current amplitude
+        amount_of_revolutions = amount_to_move*.35
+
+        #get the amount of time to speed up or slowdown to get the correct revolutions
+        wait_time = amount_of_revolutions/((self.freqency_to_motor_speed/200) - (self.freqency_to_motor_speed - 2000))
+
+        if amount_of_revolutions > 0:
+            print("slowing")
+            s0.go_until_press(s0_rotation_direction, self.freqency_to_motor_speed-2000)
+            sleep(wait_time)
+        else:
+            print("speeding up")
+            s0.go_until_press(s0_rotation_direction, self.freqency_to_motor_speed+2000)
+            sleep(wait_time * -1)
+        print("balancing speed")
+        s0.go_until_press(s0_rotation_direction, self.freqency_to_motor_speed)
+
+    def freqency_increase(self, freqency):
+        self.freqency = freqency
+        self.freqency_to_motor_speed = freqency*200
+        s0.go_until_press(s0_rotation_direction, self.freqency_to_motor_speed)
+        s1.go_until_press(s0_rotation_direction, self.freqency_to_motor_speed)
+
+
+    def live_earth_quake_data(self):
+        #https://manual.raspberryshake.org/udp.html use UDP on raspberry shake 1D
+        
+        print("does the thing")
+
+    def historical_earth_quake_simulation(self):
+
+
+        print("does the thing")
+
+
+    def speed_change(self):
 
         self.motor_0_speed = self.ids.speed_slider_1.value
 
@@ -157,27 +203,14 @@ class MainScreen(Screen):
             s1.go_until_press(s1_rotation_direction, self.ids.speed_slider_1.value)
 
     def add_10(self):
-
-        """Changes motor_0_speed to 110% of its previous speed. "motor_0_speed" is defined early in the project and
-        by running the "speed_change" function. This does not have a limit and will push the center thingy beyond its
-        limits, so be careful while using these buttons."""
-
-        self.motor_0_speed = self.motor_0_speed * 1.1
+        self.motor_0_speed = self.motor_0_speed + self.motor_0_speed * .1
         s0.go_until_press(s0_rotation_direction, int(self.motor_0_speed))
 
     def subtract_10(self):
-
-        """Changes motor_0_speed to 90% of its previous speed. "motor_0_speed" is defined early in the project and
-        by running the "speed_change" function. This does not have a limit and will push the center thingy beyond its
-        limits, so be careful while using these buttons."""
-
-        self.motor_0_speed = self.motor_0_speed * .9
+        self.motor_0_speed = self.motor_0_speed - self.motor_0_speed * .1
         s0.go_until_press(s0_rotation_direction, int(self.motor_0_speed))
 
     def soft_stop(self):
-
-        """Soft stops both motors."""
-
         s0.softStop()
         print("s0: stopping!")
 
@@ -186,9 +219,6 @@ class MainScreen(Screen):
 
     @staticmethod
     def exit_program():
-
-        """Frees all steppers, closes cyprus, cleans up the GPIO, and then calls the "quit()" functions."""
-
         s0.free_all()
         cyprus.close()
         GPIO.cleanup()
@@ -199,33 +229,27 @@ class MainScreen(Screen):
     """
         # Adjust spiral rotating disk posistion to control amplitude
         # adjust disk rotate speed in sync to adjust freqency
-
         # equation for amplitude can be calculated by manually applying a gradient along the sprial
         # using that as the variable value and multiplying that by the change constant which is the
         # amount the spiral shrinks in radius with each loop with max diameter of the spiral and the
         # calibration to get the posistion we can use that to caculate the amplitude
-
         def update_current_position():
             # make the color gradient corespond to apmpited then convert amplitude to poition on the spiral
             self.current_position = self.color_gradient_color * self.sclar_that_realtes_color_gradient_to_position
             on
             spiral
-
         def sekiton_function_amplitude_control(amplitude):
             # convert desired ampliude to position on spiral and use position control to move the spiral to desired position
             self.current_amplitude = self.max_radius - (
                         self.current_position * self.radius_decrease_per_position_change)
-
         # increase or decrease speed to adjust the position of the disk on the spiral
         def position_control(position):
-
         # calculate the distance traveled during the aceleration set the aceleration value constant with the stepper motor
         # set a value that increases or decreases speed by x value befre hand and increases or decreases speed when adjusting disk position
         # using the calculated distance traveled during aceleration/deceleration and convert that to am amplitude change then set that as a minimum change in amplitudeto make a change
         # set up a y = m*(x-(a+b)) function
         # where y is time, m is the distance traveled after reaching speed, x is desired distance to position on spiral,
         # a is distance change when accelerating, and b is distance change when decelerating
-
         def frequency_control():
     # increase or decrease speed to increase freqency 
     """
